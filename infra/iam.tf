3d63d470bc34cf3beb771cea63a1bf048ecd5a77
capstne <temi.oluseun@gmail.com>
2026-01-09 14:27:30 -0500

    added Iam roles and policies for glue and step functions

---
+++ infra/iam.tf
@@ -0,0 +1,122 @@
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+
+data "aws_iam_policy_document" "glue_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "glue_role" {
+  name               = "${local.name}-glue-role"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume.json
+  tags               = local.tags
+}
+
+data "aws_iam_policy_document" "glue_policy" {
+  statement {
+    sid = "S3Access"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      aws_s3_bucket.raw.arn,
+      "${aws_s3_bucket.raw.arn}/*",
+      aws_s3_bucket.curated.arn,
+      "${aws_s3_bucket.curated.arn}/*",
+      aws_s3_bucket.artifacts.arn,
+      "${aws_s3_bucket.artifacts.arn}/*"
+    ]
+  }
+
+  statement {
+    sid = "GlueCatalogAccess"
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:CreateTable",
+      "glue:UpdateTable",
+      "glue:BatchCreatePartition",
+      "glue:BatchUpdatePartition"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "CloudWatchLogs"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "glue_inline" {
+  name   = "${local.name}-glue-inline"
+  role   = aws_iam_role.glue_role.id
+  policy = data.aws_iam_policy_document.glue_policy.json
+}
+
+# ---- Step Functions role ----
+
+data "aws_iam_policy_document" "sfn_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["states.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "sfn_role" {
+  name               = "${local.name}-sfn-role"
+  assume_role_policy = data.aws_iam_policy_document.sfn_assume.json
+  tags               = local.tags
+}
+
+data "aws_iam_policy_document" "sfn_policy" {
+  statement {
+    sid = "GlueStartJobRun"
+    actions = [
+      "glue:StartJobRun",
+      "glue:GetJobRun",
+      "glue:GetJobRuns",
+      "glue:GetJob"
+    ]
+    resources = [
+      aws_glue_job.raw_to_curated.arn,
+      aws_glue_job.curated_to_rds.arn
+    ]
+  }
+
+  statement {
+    sid = "GlueCrawler"
+    actions = [
+      "glue:StartCrawler",
+      "glue:GetCrawler"
+    ]
+    resources = [
+      aws_glue_crawler.raw.arn
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "sfn_inline" {
+  name   = "${local.name}-sfn-inline"
+  role   = aws_iam_role.sfn_role.id
+  policy = data.aws_iam_policy_document.sfn_policy.json
+}
