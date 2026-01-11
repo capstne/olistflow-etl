1b8d52c84b55ada5f1cc86589e53aad1602377bf
capstne <temi.oluseun@gmail.com>
2026-01-11 15:41:57 -0500

    updated insatnce type

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -48,7 +48,7 @@ resource "aws_key_pair" "bastion" {
 
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.windows.id
-  instance_type               = "t4g.nano" 
+  instance_type               = "t3.medium" 
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.bastion.id]
   associate_public_ip_address = true
