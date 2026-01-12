5dbd0331834d5f3ff9f2b339ee0799747132c734
capstne <temi.oluseun@gmail.com>
2026-01-12 00:29:13 -0500

    fixed file path, windows script

--- scripts/windows/windows-userdata.ps1
+++ scripts/windows/windows-userdata.ps1
@@ -1,4 +1,3 @@
-<powershell>
 # Run as System on startup
 $Admin = [adsi]("WinNT://./administrator,user")
 $Admin.SetPassword("${admin_password}")
@@ -7,4 +6,3 @@ $Admin.SetInfo()
 # Enable RDP
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-</powershell>
