9aaaa2e11de52fa2213f99f841978847d8efabd2
capstne <temi.oluseun@gmail.com>
2026-01-20 14:58:02 -0500

    added script for copying s3 files into bastion

--- scripts/windows/windows-userdata.ps1
+++ scripts/windows/windows-userdata.ps1
@@ -15,3 +15,9 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 # install pgadmin4 with chocolately
 choco install pgadmin4 -y
 
+# import aws powershell module for reading server files from s3 bucket
+Import-Module AWSPowerShell
+
+# read servers.json, init.sql files and store them in pgadmin folder
+Read-S3Object -BucketName "olistflow-etl-dev-artifacts" -Key "pgadmin/servers.json" -File "C:\pgadmin\servers.json"
+Read-S3Object -BucketName "olistflow-etl-dev-artifacts" -Key "scripts/sql/init.sql" -File "C:\pgadmin\scripts\sql\init.sql"
