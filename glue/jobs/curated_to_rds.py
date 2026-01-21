1365bd7e3898932dc8db78ba2bf61ddeb99a1cee
capstne <temi.oluseun@gmail.com>
2026-01-21 16:37:41 -0500

    cleaned up dead code

--- glue/jobs/curated_to_rds.py
+++ glue/jobs/curated_to_rds.py
@@ -10,10 +10,7 @@ from pyspark.sql import functions as F
 args = getResolvedOptions(sys.argv, [
     'JOB_NAME',
     'CURATED_DB',
-    'JDBC_CONNECTION_NAME',
-    'URL',
-    'USERNAME',
-    'PASSWORD'
+    'JDBC_CONNECTION_NAME'
 ])
 
 sc = SparkContext()
@@ -27,44 +24,14 @@ curated_orders = glueContext.create_dynamic_frame.from_catalog(
     table_name="orders"
 ).toDF()
 
-
-glueContext.write_dynamic_frame.from_options(
+glueContext.write_dynamic_frame.from_jdbc_conf(
     frame=DynamicFrame.fromDF(curated_orders, glueContext, "df"),
-    connection_type="postgresql",
+    catalog_connection=args['JDBC_CONNECTION_NAME'],
     connection_options={
-        "connectionName": args["JDBC_CONNECTION_NAME"],
-        "url": args["URL"],
-        "username": args["USERNAME"],
-        "password": args["PASSWORD"],
-        "dbtable": "fact_orders",
         "database": "postgres",
-        "preactions": "TRUNCATE TABLE fact_orders"
+        "dbtable": "olistflow.fact_orders",
+        "preactions": "TRUNCATE TABLE fact_orders;"
     }
 )
-
-## Write to RDS Postgres via Glue JDBC connection (truncate + load).
-# curated_orders.write \
-#     .format("jdbc") \
-#     .option("url", "jdbc:postgresql://olistflow-etl-dev.ct6v1kobwsnl.us-east-1.rds.amazonaws.com:5432/postgres") \
-#     .option("connectionName", args['JDBC_CONNECTION_NAME']) \
-#     .option("dbtable", "fact_orders") \
-#     .option("truncate", "true") \
-#     .mode("overwrite") \
-#     .save()
-
-# # Also write dims (simple extracts).
-# dims = curated_orders.select(
-#     "customer_id", "customer_unique_id", "customer_zip_code_prefix",
-#     "customer_city", "customer_state"
-# ).distinct()
-
-# dims.write \
-#     .format("jdbc") \
-#     .option("url", "jdbc:postgresql://your-rds-endpoint:5432/your_db_name") \ 
-#     .option("connectionName", args['JDBC_CONNECTION_NAME']) \
-#     .option("dbtable", "dim_customers") \
-#     .option("truncate", "true") \
-#     .mode("overwrite") \
-#     .save()
 
 job.commit()
