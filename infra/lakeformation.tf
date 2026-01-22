1afbe8cc101f5fcae78678cfc2818bf2e238562f
capstne <temi.oluseun@gmail.com>
2026-01-22 09:46:43 -0500

    added lf.tf

---
+++ infra/lakeformation.tf
@@ -0,0 +1,20 @@
+# removes all lake formation db permissions for provisioned glue dbs 
+resource "aws_lakeformation_permissions" "remove_db_permissions" {
+  principal = aws_iam_role.glue_role.arn
+
+  permissions = []  # empty = removes all LF permissions
+
+  database {
+    name = aws_glue_catalog_database.raw.name
+  }
+}
+
+resource "aws_lakeformation_permissions" "remove_db_permissions" {
+  principal = aws_iam_role.glue_role.arn
+
+  permissions = []  # empty = removes all LF permissions
+
+  database {
+    name = aws_glue_catalog_database.curated.name
+  }
+}
