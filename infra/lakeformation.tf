ed820ac560c6dc1873dd5b75a007e33176fbf951
capstne <temi.oluseun@gmail.com>
2026-01-22 10:20:05 -0500

    makes glue role datalake admin

--- infra/lakeformation.tf
+++ infra/lakeformation.tf
@@ -1,4 +1,11 @@
-# adds "ALTER" and "DESCRIBE" lake formation db permissions for provisioned glue dbs 
+# makes glue role datalake admin, adds "ALTER" and "DESCRIBE" lake formation db permissions for provisioned glue dbs 
+
+resource "aws_lakeformation_data_lake_settings" "admin" {
+  admins = [
+    aws_iam_role.glue_role.arn
+  ]
+}
+
 resource "aws_lakeformation_permissions" "remove_raw_db_permissions" {
   principal = aws_iam_role.glue_role.arn
 
@@ -7,6 +14,11 @@ resource "aws_lakeformation_permissions" "remove_raw_db_permissions" {
   database {
     name = aws_glue_catalog_database.raw.name
   }
+
+  depends_on = [
+    aws_lakeformation_data_lake_settings.admin,
+    aws_glue_catalog_database.raw
+  ]
 }
 
 resource "aws_lakeformation_permissions" "remove_curated_db_permissions" {
@@ -17,4 +29,9 @@ resource "aws_lakeformation_permissions" "remove_curated_db_permissions" {
   database {
     name = aws_glue_catalog_database.curated.name
   }
+
+  depends_on = [
+    aws_lakeformation_data_lake_settings.admin,
+    aws_glue_catalog_database.curated
+  ]
 }
