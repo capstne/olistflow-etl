d502756a4916381548a0555a933b2359c5d063ff
capstne <temi.oluseun@gmail.com>
2026-01-23 16:47:34 -0500

    minor refactoring

--- glue/jobs/curated_to_rds.py
+++ glue/jobs/curated_to_rds.py
@@ -33,7 +33,7 @@ logger = glue_context.get_logger()
 job = Job(glue_context)
 job.init(args['JOB_NAME'], args)
 
-def func_write_dynamic_frame(my_glue_context, my_dataframe, my_catalog_connection, my_destination_table):
+def func_write_dynamic_frame(my_glue_context, my_dataframe, my_catalog_connection, my_destination_table, my_host):
     dbtable = 'olistflow.{}'.format(my_destination_table)
     try:
         logger.info('Creating a DynamicFrame from DataFrame ({0}).'.format(my_dataframe))
@@ -44,7 +44,7 @@ def func_write_dynamic_frame(my_glue_context, my_dataframe, my_catalog_connectio
             frame=my_dynamic_frame,
             catalog_connection=my_catalog_connection,
             connection_options={
-                'database': 'postgres',
+                'database': my_host,
                 'dbtable': dbtable
             }
         )
@@ -74,26 +74,39 @@ def func_get_connection_properties(my_glue_client):
     except Exception as e:
         logger.error('Error: {0}'.format(e))
 
-def func_truncate_all_tables(my_connection_properties, my_tables):
-    logger.info('Starting truncate_all_tables function')
+def format_connection_details(my_connection_properties):
+    logger.info('Extracting connection credentials from DB connection.')
+    try:
+        jdbc_url = my_connection_properties['JDBC_CONNECTION_URL']
+        user     = my_connection_properties.get('USERNAME')
+        password = my_connection_properties.get('PASSWORD') 
+        
+        parsed = urlparse(jdbc_url.replace('jdbc:', '', 1))
+        host = parsed.hostname
+        port = parsed.port
+        dbname = parsed.path.lstrip('/')  # 'postgres'
 
-    jdbc_url = my_connection_properties['JDBC_CONNECTION_URL']
-    user     = my_connection_properties.get('USERNAME')
-    password = my_connection_properties.get('PASSWORD') 
-    
-    parsed = urlparse(jdbc_url.replace('jdbc:', '', 1))
-    host = parsed.hostname
-    port = parsed.port
-    dbname = parsed.path.lstrip('/')  # 'postgres'
-    
+        db_connection_credentials = {
+            'host': host,
+            'dbname': dbname,
+            'user': user,
+            'password': password,
+            'port': port, 
+        }
+        return db_connection_credentials
+    except Exception as e:
+        logger.error('Error: {0}'.format(e))
+
+def func_truncate_all_tables(my_formatted_connection_creds, my_tables):
+    logger.info('Starting truncate_all_tables function.')
     logger.info('Connecting to DB...')
     try:
         conn = psycopg2.connect(
-            host=host,
-            dbname=dbname,
-            user=user,
-            password=password,
-            port=port,
+            host=my_formatted_connection_creds['host'],
+            dbname=my_formatted_connection_creds['dbname'],
+            user=my_formatted_connection_creds['user'],
+            password=my_formatted_connection_creds['password'],
+            port=my_formatted_connection_creds['port'],
         )
         
         conn.autocommit = True
@@ -108,9 +121,10 @@ def func_truncate_all_tables(my_connection_properties, my_tables):
         
 glue_client = boto3.client('glue')
 db_connection_properties = func_get_connection_properties(glue_client)
+formatted_connection_creds = format_connection_details(db_connection_properties)
 
 # truncate all tables
-func_truncate_all_tables(db_connection_properties, db_tables)
+func_truncate_all_tables(formatted_connection_creds, db_tables)
 
 # load all dimensions tables first
 dim_customers = func_get_dataframe(glue_context, args['RAW_DB'], 'olist_customers_dataset_csv')
@@ -118,7 +132,7 @@ dim_customers = dim_customers \
   .withColumnRenamed('customer_zip_code_prefix','zip_code_prefix') \
   .withColumnRenamed('customer_city','city') \
   .withColumnRenamed('customer_state','state')
-func_write_dynamic_frame(glue_context, dim_customers, args['JDBC_CONNECTION_NAME'], db_tables['customers'])
+func_write_dynamic_frame(glue_context, dim_customers, args['JDBC_CONNECTION_NAME'], db_tables['customers'], formatted_connection_creds['host'])
 
 dim_products = func_get_dataframe(glue_context, args['RAW_DB'], 'olist_products_dataset_csv')
 dim_products = dim_products['product_id', 'product_category_name']
