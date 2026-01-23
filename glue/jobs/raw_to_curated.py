4bdf192c182e7e69c3633d80719233cfa1a8e2f4
capstne <temi.oluseun@gmail.com>
2026-01-23 11:46:28 -0500

    addressed error

--- glue/jobs/raw_to_curated.py
+++ glue/jobs/raw_to_curated.py
@@ -1,111 +1,122 @@
-import boto3
+import awswrangler as wr
 import sys
-import psycopg2
-
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
-from awsglue.dynamicframe import DynamicFrame
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql import functions as F
-from pyspark.sql.functions import col, to_date
-from urllib.parse import urlparse
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 args = getResolvedOptions(sys.argv, [
     'JOB_NAME',
-    'CURATED_DB',
-    'RAW_DB',
-    'JDBC_CONNECTION_NAME'
+    'RAW_BUCKET', 'RAW_PREFIX',
+    'CURATED_BUCKET', 'CURATED_PREFIX',
+    'RAW_DB', 'CURATED_DB'
 ])
 
-db_tables = {
-    'customers': 'dim_customers',
-    'products': 'dim_products',
-    'sellers': 'dim_sellers',
-    'orders': 'fact_orders',
-}
-
 sc = SparkContext()
-glue_context = GlueContext(sc)
-job = Job(glue_context)
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-def func_write_dynamic_frame(my_glue_context, my_dataframe, my_catalog_connection, my_destination_table):
-    my_glue_context.write_dynamic_frame.from_jdbc_conf(
-        frame=DynamicFrame.fromDF(my_dataframe, my_glue_context),
-        catalog_connection=my_catalog_connection,
-        connection_options={
-            "database": "postgres",
-            "dbtable": "olistflow.{}".format(my_destination_table)
-        }
+# Load via GlueContext (bypasses Spark catalog issues)
+raw_orders_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_orders_dataset_csv"
+).toDF()
+
+raw_customers_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_customers_dataset_csv"
+).toDF()
+
+raw_items_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_order_items_dataset_csv"
+).toDF()
+
+raw_products_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_products_dataset_csv"
+).toDF()
+
+raw_payments_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_order_payments_dataset_csv"
+).toDF()
+
+raw_reviews_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_order_reviews_dataset_csv"
+).toDF()
+
+raw_sellers_df = glueContext.create_dynamic_frame.from_catalog(
+    database=args['RAW_DB'],
+    table_name="olist_sellers_dataset_csv"
+).toDF()
+
+# Add order_date partition column (parse from order_purchase_timestamp).
+raw_orders_df = raw_orders_df.withColumn(
+    "order_date",
+    to_date(col("order_purchase_timestamp"))
+)
+
+raw_sellers_df = raw_sellers_df.withColumnRenamed("seller_id", "raw_seller_id")
+
+# Curated orders (fact seed): join customers, items, products, payments, reviews, sellers, aggregate.
+temp_joined = raw_orders_df \
+    .join(raw_customers_df, "customer_id", "left") \
+    .join(raw_items_df, "order_id", "left") \
+    .join(raw_products_df, "product_id", "left") \
+    .join(raw_payments_df, "order_id", "left") \
+    .join(raw_reviews_df, "order_id", "left") \
+    .join(raw_sellers_df, col("seller_id") == raw_sellers_df["raw_seller_id"], "left") 
+
+# Minimal groupBy: order_id + order_date, pick first of each (validates joins work).
+curated_orders = temp_joined \
+    .groupBy("order_id", "order_date") \
+    .agg(
+        first("customer_id").alias("customer_id"),
+        first("customer_unique_id").alias("customer_unique_id"),
+        first("customer_zip_code_prefix").alias("customer_zip_code_prefix"),
+        first("customer_city").alias("customer_city"),
+        first("customer_state").alias("customer_state"),
+        first("price").alias("total_price"),  
+        first("freight_value").alias("total_freight"),
+        first("payment_installments").alias("installments"),
+        first("payment_sequential").alias("payment_count"),
+        first("order_status").alias("order_status"),
+        first("review_score").alias("review_score")
+    ) \
+    .select(
+        col("order_id"),
+        col("order_date").cast(DateType()),
+        col("customer_id"),
+        col("customer_unique_id"),
+        col("customer_zip_code_prefix").cast(IntegerType()),
+        col("customer_city"),
+        col("customer_state"),
+        col("total_price").cast(DoubleType()),
+        col("total_freight").cast(DoubleType()),
+        col("installments").cast(IntegerType()),
+        col("payment_count").cast(IntegerType()),
+        col("order_status"),
+        col("review_score").cast(FloatType())
     )
-    
-def func_get_dataframe(my_glue_context, my_database, my_table):
-    return my_glue_context.create_dynamic_frame.from_catalog(
-        database=my_database,
-        table_name=my_table
-    ).toDF()
 
-def func_get_connection_properties(my_glue_client):
-    resp = my_glue_client.get_connection(
-        Name=args["JDBC_CONNECTION_NAME"],
-        HidePassword=False
-    )
-    props = resp["Connection"]["ConnectionProperties"]
-    return props
+# convert to pandas
+curated_orders_p_df = curated_orders.toPandas()
 
-def func_truncate_all_tables(my_connection_properties, my_tables):
-    jdbc_url = my_connection_properties["JDBC_CONNECTION_URL"]
-    user     = my_connection_properties.get("USERNAME")
-    password = my_connection_properties.get("PASSWORD") 
-    
-    parsed = urlparse(jdbc_url.replace("jdbc:", "", 1))
-    host = parsed.hostname
-    port = parsed.port
-    dbname = parsed.path.lstrip("/")  # "postgres"
-    
-    conn = psycopg2.connect(
-        host=host,
-        dbname=dbname,
-        user=user,
-        password=password,
-        port=port,
-    )
-    
-    conn.autocommit = True
-    
-    sql_statement = """ TRUNCATE TABLE olistflow.{0}, olistflow.{1}, olistflow.{2}, olistflow.{3} RESTART IDENTITY;
-        """.format(my_tables['orders'], my_tables['sellers'], my_tables['products'], my_tables['customers'])
-    
-    with conn.cursor() as cur:
-        cur.execute(sql_statement) 
-        
-glue_client = boto3.client("glue")
-db_connection_properties = func_get_connection_properties(glue_client)
-
-# truncate all tables
-func_truncate_all_tables(db_connection_properties, db_tables)
-
-# load all dimensions tables first
-dim_customers = func_get_dataframe(glue_context, args['RAW_DB'], 'olist_customers_dataset_csv')
-dim_customers = dim_customers \
-  .withColumnRenamed("customer_zip_code_prefix","zip_code_prefix") \
-  .withColumnRenamed("customer_city","city") \
-  .withColumnRenamed("customer_state","state")
-func_write_dynamic_frame(glue_context, dim_customers, args['JDBC_CONNECTION_NAME'], db_tables['customers'])
-
-dim_products = func_get_dataframe(glue_context, args['RAW_DB'], 'olist_products_dataset_csv')
-dim_products = dim_products['product_id', 'product_category_name']
-func_write_dynamic_frame(glue_context, dim_products, args['JDBC_CONNECTION_NAME'], db_tables['products'])
-
-dim_sellers = func_get_dataframe(glue_context, args['RAW_DB'], 'olist_sellers_dataset_csv')
-func_write_dynamic_frame(glue_context, dim_sellers, args['JDBC_CONNECTION_NAME'], db_tables['sellers'])
-
-# then load the fact table
-curated_orders = func_get_dataframe(glue_context, args['CURATED_DB'], 'orders')
-curated_orders = curated_orders.withColumn("order_date", to_date(col("order_date"), "yyyy-MM-dd")) 
-
-func_write_dynamic_frame(glue_context, curated_orders, args['JDBC_CONNECTION_NAME'], db_tables['orders'])
+wr.s3.to_parquet(
+    df=curated_orders_p_df,
+    path=f"s3://{args['CURATED_BUCKET']}/{args['CURATED_PREFIX']}orders/",
+    dataset=True,
+    database=args['CURATED_DB'],
+    table="orders",
+    mode="overwrite",
+    partition_cols=['order_date']
+)
 
 job.commit()
