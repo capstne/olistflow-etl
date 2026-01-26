70f9be8e02249058370ae0ae3f03464ea0e54cca
capstne <temi.oluseun@gmail.com>
2026-01-26 11:29:50 -0500

    made sure every admin has valid arn

--- infra/lakeformation.tf
+++ infra/lakeformation.tf
@@ -2,16 +2,21 @@ data "aws_lakeformation_data_lake_settings" "current" {
   catalog_id = data.aws_caller_identity.current.account_id
 }
 
-# locals {
-#   lf_admins = distinct(concat(
-#     tolist(data.aws_lakeformation_data_lake_settings.current.admins),
-#     [aws_iam_role.glue_role.arn]
-#   ))
-# }
+locals {
+  existing_admins = [
+    for a in tolist(data.aws_lakeformation_data_lake_settings.current.admins) :
+    a if can(regex("^arn:", a))
+  ]
+
+  # lf_admins = distinct(concat(
+  #   local.existing_admins,
+  #   [aws_iam_role.glue_role.arn]
+  # ))
+}
 
 resource "aws_lakeformation_data_lake_settings" "this" {
   catalog_id = data.aws_caller_identity.current.account_id
-  admins     = tolist(data.aws_lakeformation_data_lake_settings.current.admins)
+  admins     = local.existing_admins
 
   create_database_default_permissions {}
   create_table_default_permissions  {}
